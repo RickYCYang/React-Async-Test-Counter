6b0b6c53437a4bbf9ffe7da2dc2a73ce3ad34c7d
Rick <rickyang2910@gmail.com>
2021-09-30 22:47:01 +0800

    Add redux-persist to keep state in localStorage

--- src/Redux/Store.ts
+++ src/Redux/Store.ts
@@ -1,10 +1,25 @@
-import { createStore, applyMiddleware } from 'redux';
-import createSagaMiddleware from 'redux-saga';
-import Counter from './Reducers/CounterReducer';
-import thunk from 'redux-thunk';
-import saga from './Sagas/saga';
+import { createStore, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
+import Counter from "./Reducers/CounterReducer";
+import thunk from "redux-thunk";
+import saga from "./Sagas/saga";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, Counter);
+
 const sagaMiddleware = createSagaMiddleware();
-const store = createStore(Counter, applyMiddleware(thunk, sagaMiddleware));
+const store = createStore(
+  persistedReducer,
+  applyMiddleware(thunk, sagaMiddleware)
+);
 sagaMiddleware.run(saga);
+
 export type AppDispatch = typeof store.dispatch;
+export const persistor = persistStore(store);
 export default store;
--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import Store from './Redux/Store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import Store, { persistor } from "./Redux/Store";
+import { Provider } from "react-redux";
+import * as serviceWorker from "./serviceWorker";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={Store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
